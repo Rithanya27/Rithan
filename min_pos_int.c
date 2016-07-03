9f2a6268f46012f4649df98851b953ecfdd0401d
Rithanya27 <rithanya27@gmail.com>
2016-07-03 21:39:16 +0530

    Create min_pos_int.c

---
+++ min_pos_int.c
@@ -0,0 +1,22 @@
+# odd
+#include <stdio.h>
+void odd(int a)
+{
+	if(a%2==1)
+	{
+		printf("the min +ve no is %d",a);
+	}
+}
+main()
+{
+int a,n,i;
+printf("Enter number");
+scanf("%d",&n);
+for(i=0;i<n-1;i++);
+{
+	if(n/i==a);
+	{
+		odd(a);
+	}
+}
+}
